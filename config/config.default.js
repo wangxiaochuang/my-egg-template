9630d89a6fdcc6f9539e76e9173f56b3b2f0a722
jackstraw <jackstraw@jackstraws-Mac-mini.local>
2018-01-17 10:22:30 +0800

    use middleware

--- config/config.default.js
+++ config/config.default.js
@@ -10,3 +10,12 @@ exports.news = {
   pageSize: 5,
   serverUrl: 'https://hacker-news.firebaseio.com/v0'
 }
+
+exports.middleware = [
+  'robot'
+]
+exports.robot = {
+  ua: [
+    /Baiduspider/i
+  ]
+}
